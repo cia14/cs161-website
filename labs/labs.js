045f4ca4b47eac61f1d53e4811c4302b4f02c7ac
Kiri Wagstaff <wkiri@wkiri.com>
2020-01-01 16:21:16 -0800

    Update formatting of a.m. and p.m.

--- labs/labs.js
+++ labs/labs.js
@@ -84,11 +84,11 @@ var times = [
         var ampm = "";
         if (hour > 1300) {
             twelveHour -= 1200;
-            ampm = "pm";
+            ampm = " p.m.";
         } else if (hour <= 1300 && hour >= 1200) {
-            ampm = "pm";
+            ampm = " p.m.";
         } else {
-            ampm = "am";
+            ampm = " a.m.";
         }
         var hours = Math.floor(twelveHour / 100)
         var minutes = twelveHour - (hours * 100);
@@ -99,7 +99,7 @@ var times = [
     }
     
     var table = '<table class="labsTable">';
-    table += "<tr><th>Section</th><th>Day</th><th>Time</th><th colspan='3'>TA's</th></tr>";
+    table += "<tr><th>Section</th><th>Day</th><th>Time</th><th colspan='3'0%>TAs</th></tr>";
 
     for (var x = 0; x < times.length; x++) {
         table += "<tr>";
