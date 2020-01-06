8db732456dd94e846cf9a16facb8f487fe22b73c
Kiri Wagstaff <wkiri@yahoo.com>
2020-01-05 19:04:54 -0800

    Update comments to follow our style guide.

--- assignments/assign1_fortune.cpp
+++ assignments/assign1_fortune.cpp
@@ -12,15 +12,21 @@ using namespace std;
 
 int main() 
 {
-  // Declare variables.  For each one, include
-  // (1) why you chose the data type for the variable
-  // (2) the minimum and maximum value that it can hold
+  /*
+   * Declare variables.  For each one, include
+   * (1) why you chose the data type for the variable
+   * (2) the minimum and maximum value that it can hold.
+   */
 
-  // Prompt the user to enter a value for each number,
-  // and store it in the appropriate variable
+  /*
+   * Prompt the user to enter a value for each number,
+   * and store it in the appropriate variable.
+   */
 
-  // Print out the user's fortune with blanks
-  // filled in by the numbers they entered
+  /*
+   * Print out the user's fortune with blanks
+   * filled in by the numbers they entered.
+   */
 
   return 0;
 }
