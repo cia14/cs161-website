f6bf6387b0ef256e52ac46f7ed29f0f95305f8b7
Kiri Wagstaff <wkiri@yahoo.com>
2020-01-05 19:03:35 -0800

    Add template for Assignment 1.

---
+++ assignments/assign1_fortune.cpp
@@ -0,0 +1,26 @@
+/*****************************************************
+ ** Program: 
+ ** Author: 
+ ** Date: 
+ ** Description: 
+ ** Input:  
+ ** Output: 
+ *****************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+  // Declare variables.  For each one, include
+  // (1) why you chose the data type for the variable
+  // (2) the minimum and maximum value that it can hold
+
+  // Prompt the user to enter a value for each number,
+  // and store it in the appropriate variable
+
+  // Print out the user's fortune with blanks
+  // filled in by the numbers they entered
+
+  return 0;
+}
