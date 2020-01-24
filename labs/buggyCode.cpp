d903359d4a4a2432075413feeea486b94165a465
Kiri Wagstaff <wkiri@yahoo.com>
2020-01-24 15:44:50 -0800

    Lab 4 buggy code example.

---
+++ labs/buggyCode.cpp
@@ -0,0 +1,45 @@
+/*
+THERE ARE BUGS IN THE FOLLOWING CODE.
+*/
+#include <iostraem>
+
+
+int main(){
+
+	int x=3, y;
+	char myAnswer;
+
+	y = x;
+	cout << "y is set to: " << y << endl;
+
+	cout << "Please input a number: ";
+	cin << y;
+	again = true
+	while(again){
+		if(x > y)
+			cout << "X is greater than Y\n";
+			cout << would you like to input another number? >> endl;
+			cin >> again;
+		else{
+			cout << "X is less than Y" << endl;
+			cout << "would you like to input another number?" >> endl;
+			cin >> again;
+		}
+	}
+	for(x = 0; x < 10; x+1)
+		cout << x << endl;
+	
+	cout << "What number would you like to find the factorial for? " << endl;
+	cin >> num
+	for(int x = num; x > 0; x--)
+		x = x * x;
+
+	cout << "Are you enjoying cs161? (y or n) " << endl;
+	cin >> myanswer;
+	if(myanswer = y)
+		cout << "YAY" << endl;
+	else
+		cout << "YOU WILL SOON!" << endl;
+
+	return 0;
+
